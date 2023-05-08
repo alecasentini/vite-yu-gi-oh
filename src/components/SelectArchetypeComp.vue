abbde1ebdc9af0dd87e9682113a4a07b23e4d4f9
alecasentini <ale.casentini@gmail.com>
2023-05-08 14:25:27 +0200

    Aggiunte option di tutti gli archetipi alla select

--- src/components/SelectArchetypeComp.vue
+++ src/components/SelectArchetypeComp.vue
@@ -1,15 +1,28 @@
 <script>
+import axios from 'axios';
 
 export default {
     name: "SelectArchetypeComp",
-
+    data() {
+        return {
+            archetypes: []
+        }
+    },
+    created() {
+        axios.get('https://db.ygoprodeck.com/api/v7/archetypes.php')
+            .then(res => {
+                for (let i = 0; i < res.data.length; i++) {
+                    this.archetypes.push(res.data[i]);
+                }
+            })
+    }
 }
 </script>
 
 <template>
     <select class="form-select" aria-label="Default select example">
         <option selected>Select Archetype</option>
-        <option value=""></option>
+        <option v-for="archetype in archetypes" :value="archetype.archetype_name">{{ archetype.archetype_name }}</option>
 
     </select>
 </template>
